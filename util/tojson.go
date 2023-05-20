16729d16157f3f304147b2c955f0a570c825fb17
claustra01 <shinya430.g@gmail.com>
2023-05-21 01:47:18 +0900

    add: string to json

---
+++ util/tojson.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"encoding/json"
+	"log"
+)
+
+type MsgFromClient struct {
+	StreamId string `json:"stream_id"`
+	Comment  string `json:"comment"`
+	Reaction bool   `json:"reaction"`
+}
+
+func StringToJson(rawMsg string) MsgFromClient {
+	jsonBytes := []byte(rawMsg)
+	var jsonMsg MsgFromClient
+	if err := json.Unmarshal(jsonBytes, &jsonMsg); err != nil {
+		log.Fatal(err)
+	}
+	return jsonMsg
+}
